6d6d3a0e080efcb942e414b51ebb9ac2e0d5d396
David Duong <dduong@csumb.edu>
2020-03-10 22:11:49 -0700

    Part 2 Done, constraint issues

--- parstigram/SceneDelegate.swift
+++ parstigram/SceneDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
@@ -18,6 +19,15 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
+        
+        if PFUser.current() != nil {
+        
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        //self.window = UIWindow(windowScene: windowScene)
+        self.window?.rootViewController = storyboard.instantiateViewController(withIdentifier: "FeedNavigationController")
+        self.window?.makeKeyAndVisible()
+            
+        }
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
